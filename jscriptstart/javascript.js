583352ebaf75116c08271b1eebd445bc5d4e9375
Aidan2426 <aash3@hawk.iit.edu>
2025-01-09 22:57:31 -0500

    Javascript

---
+++ jscriptstart/javascript.js
@@ -0,0 +1,142 @@
+let name = "john";
+let surname = "Doe";
+
+console.log(surname);
+console.log(name);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age); 
+
+const pi = 3.14;
+
+//pi = 10
+
+console.log(pi);
+
+//There's also var, but it isn't really used anymore
+
+(3+2)-76*(1+1);
+
+console.log("My name's aidan, i just want to run in vs code");
+
+console.log(23+97);
+
+(5)+(5)+(5)+(5)+(5)+(5);
+
+console.log((4+6+9)/77);
+
+let a = 10;
+
+console.log(a);
+
+console.log(a*9);
+
+let b = 7*a;
+
+console.log(b);
+
+let max = 57;
+
+actual = max-13;
+
+percentage = actual/max;
+
+console.log(percentage);
+
+const string = "This is a string"; //string is just the name
+
+console.log(string)
+
+const badString = string; //this is just restoring a variable 
+
+console.log(badString);
+
+console.log(string + "horray");
+
+const newline = "This is a very long \n string that is long";
+
+console.log(newline); //js is case sensitive
+
+let x = 34
+
+let y = "this is a string"
+
+console.log(x+y);
+
+console.log(typeof y);
+
+let length = y.length;
+
+//let char = y.chatAt(0); broken
+
+let letter = y[0];
+
+let part = y.slice(1,5);
+
+let part2 = y.substring(1,5);
+
+let text1 = y.toUpperCase();
+
+let text2 = y.toLowerCase();
+
+//theres a lot more but i can't be asked
+//only reapply importatn are repalce and slice
+
+//comparisons
+
+console.log(2>1);
+console.log(2==1);
+console.log(2!=1);
+
+let result = 5>4;
+console.log(result);
+
+if(5>4){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+if(4>5){
+    console.log(true);
+}
+
+else if(5>4){
+    console.log(true);
+}
+
+else{
+    console.log(false);
+}
+
+console.log(true || true);
+console.log(true && false);
+console.log(!true);
+
+let g = 2+2;
+
+switch(g){
+    case 3:
+        console.log("too small");
+        break;
+    case 4: 
+        console.log("right");
+        break;
+    case 5: 
+        console.log("too big");
+        break;
+    default:
+        console.log("i don't know");
+}
+
+
+
+        
+
+
+
