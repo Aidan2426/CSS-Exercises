418f3fada13c55dc75f1af9f8222c21f2a72e108
Aidan2426 <aash3@hawk.iit.edu>
2025-01-20 22:38:37 -0600

    actaullayarras

---
+++ jscriptstart/array.js
@@ -0,0 +1,28 @@
+let names = ["aidan", "camden" , "Brendan"];
+let country = [];
+country[0] = "France";
+country[1] = "USA";
+country[2] = "germany";
+
+let cars = new Array("Volve", "Elentra", "hyundai");
+let car = cars[0];
+console.log(car);
+cars[0] = "fast Car"
+
+const things = ["Minecraft", "ted", 55];//arrays are objects
+let len = things.length;
+things.sort()
+let last = cars[cars.length-1];
+
+for (let i = 0; i<things.len; i++){
+    text+= things[i]
+}
+
+things.push("Lemon"); //puts this as the last element
+
+let people = ["Aidan", "Leo", "Ash"];
+let second = people[1]
+console.log(second);
+people.pop();
+people.shift()
+people.unshift();
