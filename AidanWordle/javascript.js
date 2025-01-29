b3d108e48c6d16ada7ced15a108271c0c3b4bcf2
Aidan2426 <aash3@hawk.iit.edu>
2025-01-28 21:28:44 -0600

    more wordle changes

--- AidanWordle/javascript.js
+++ AidanWordle/javascript.js
@@ -1,56 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const words = ["aidan", "photo", "cleat", "maths"];
-    const randomIndex = Math.floor(Math.random() * words.length+1);
+    const words = ["AIDAN", "PHOTO", "CLEAT", "MATHS"];
+    const randomIndex = Math.floor(Math.random() * words.length);
     let randomWord = words[randomIndex];
     let randomChars = randomWord.split("");
 
-    let rowCount = 1;
-
-    
+    let rowCount = 0;
 
     const textBox = document.querySelector("#wordInput");
     const addButton = document.querySelector("#addButton");
     const temp = document.querySelector("#temp");
 
-    const row1 = [
-        document.querySelector("#r1b1"),
-        document.querySelector("#r1b2"),
-        document.querySelector("#r1b3"),
-        document.querySelector("#r1b4"),
-        document.querySelector("#r1b5")
-    ];
-
-    const row2 = [
-        document.querySelector("#r2b1"),
-        document.querySelector("#r2b2"),
-        document.querySelector("#r2b3"),
-        document.querySelector("#r2b4"),
-        document.querySelector("#r2b5")
-    ];
-
-    const row3 = [
-        document.querySelector("#r3b1"),
-        document.querySelector("#r3b2"),
-        document.querySelector("#r3b3"),
-        document.querySelector("#r3b4"),
-        document.querySelector("#r3b5")
-    ];
-
-    const row4 = [
-        document.querySelector("#r4b1"),
-        document.querySelector("#r4b2"),
-        document.querySelector("#r4b3"),
-        document.querySelector("#r4b4"),
-        document.querySelector("#r4b5")
-    ];
-
-    const row5 = [
-        document.querySelector("#r5b1"),
-        document.querySelector("#r5b2"),
-        document.querySelector("#r5b3"),
-        document.querySelector("#r5b4"),
-        document.querySelector("#r5b5")
+    const rows = [
+        document.querySelectorAll("#r1b1, #r1b2, #r1b3, #r1b4, #r1b5"),
+        document.querySelectorAll("#r2b1, #r2b2, #r2b3, #r2b4, #r2b5"),
+        document.querySelectorAll("#r3b1, #r3b2, #r3b3, #r3b4, #r3b5"),
+        document.querySelectorAll("#r4b1, #r4b2, #r4b3, #r4b4, #r4b5"),
+        document.querySelectorAll("#r5b1, #r5b2, #r5b3, #r5b4, #r5b5"),
+        document.querySelectorAll("#r6b1, #r6b2, #r6b3, #r6b4, #r6b5")
     ];
 
     addButton.addEventListener("click", pressed);
@@ -63,112 +30,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function pressed(){
         let guessWord = textBox.value; 
-        let letters = guessWord.split("")
-
-        guess(guessWord, letters);
-        textBox.value = "";
+        guessWord = guessWord.toUpperCase();
+        if(guessWord.length > 5 || guessWord.length<5){
+            alert("You must enter a 5 letter word");
+        }
+        else{
+            let letters = guessWord.split("")
+            guess(guessWord, letters);
+            textBox.value = "";
+        }  
     }
 
     function guess(guessWord, letters){
 
-        console.log("This is the row count " + rowCount )
+        let currentRow = rows[rowCount]
 
-        if(rowCount == 1){
             for(let i = 0; i<letters.length; i++)
             {
-                row1[i].textContent = letters[i]
+                currentRow[i].textContent = letters[i]
 
                 if(letters[i] == randomChars[i]){
-                    row1[i].style.background = "green"
+                    currentRow[i].style.background = "green"
                 }
 
                 else if(randomChars.includes(letters[i])){
-                    row1[i].style.background  = "yellow";
+                    currentRow[i].style.background  = "yellow";
                 }
 
                 else{
-                    row1[i].style.background = "red";
+                    currentRow[i].style.background = "red";
                 }
             }  
-        }
-
-        if(rowCount == 2){
-            for(let i = 0; i<letters.length; i++)
-            {
-                row2[i].textContent = letters[i]
-
-                if(letters[i] == randomChars[i]){
-                    row2[i].style.background = "green"
-                }
-
-                else if(randomChars.includes(letters[i])){
-                    row2[i].style.background  = "yellow";
-                }
-
-                else{
-                    row2[i].style.background = "red";
-                }
-            }  
-        }
-
-        if(rowCount == 3){
-            for(let i = 0; i<letters.length; i++)
-            {
-                row3[i].textContent = letters[i]
-
-                if(letters[i] == randomChars[i]){
-                    row3[i].style.background = "green"
-                }
-
-                else if(randomChars.includes(letters[i])){
-                    row3[i].style.background  = "yellow";
-                }
-
-                else{
-                    row3[i].style.background = "red";
-                }
-            }  
-        }
-
-        if(rowCount == 4){
-            for(let i = 0; i<letters.length; i++)
-            {
-                row4[i].textContent = letters[i]
-
-                if(letters[i] == randomChars[i]){
-                    row4[i].style.background = "green"
-                }
-
-                else if(randomChars.includes(letters[i])){
-                    row4[i].style.background  = "yellow";
-                }
-
-                else{
-                    row4[i].style.background = "red";
-                }
-            }  
-        }
-
-        if(rowCount == 5){
-            for(let i = 0; i<letters.length; i++)
-            {
-                row5[i].textContent = letters[i]
-
-                if(letters[i] == randomChars[i]){
-                    row5[i].style.background = "green"
-                }
-
-                else if(randomChars.includes(letters[i])){
-                    row5[i].style.background  = "yellow";
-                }
-
-                else{
-                    row5[i].style.background = "red";
-                }
-            }  
-        }
-
-        rowCount++;
-
-        }
+            rowCount++;
+        }        
 });
